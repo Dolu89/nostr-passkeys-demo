cb94ad1515552773194af3784bb40d0b0014729f
Dolu <doludev@protonmail.com>
2024-08-15 15:05:04 +0200

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import WALC from "@lo-fi/webauthn-local-client/bundlers/vite";
-import LDL from "@lo-fi/local-data-lock/bundlers/vite";
-import LV from "@lo-fi/local-vault/bundlers/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), WALC(), LDL(), LV()],
+  plugins: [vue(), WALC()],
 
   optimizeDeps: {
     esbuildOptions: {
